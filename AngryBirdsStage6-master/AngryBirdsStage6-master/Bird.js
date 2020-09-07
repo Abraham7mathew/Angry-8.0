aca810cab15ad59692ffcfdefb7c4a15d71f69ee
Abraham7mathew <68383372+Abraham7mathew@users.noreply.github.com>
2020-09-07 19:45:39 +0530

    Add files via upload

---
+++ AngryBirdsStage6-master/AngryBirdsStage6-master/Bird.js
@@ -0,0 +1,24 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.simage = loadImage("sprites/smoke.png");
+    this.path = [];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+    console.log(this.body.speed)
+    if(this.body.velocity.x > 10 && this.body.position.x > 200)
+    {
+      var pos = [this.body.position.x, this.body.position.y];
+      this.path.push(pos);
+    }
+    for(var i = 0;i < this.path.length; i++)
+    {
+      image(this.simage,this.path[i][0],this.path[i][1])
+    }
+  }
+}
